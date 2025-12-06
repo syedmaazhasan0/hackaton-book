f88478e2a0cce67aaee017e97e1047103b1797aa
syedmaazhasan0 <160649717+syedmaazhasan0@users.noreply.github.com>
2025-12-06 20:14:39 +0500

    Fix: Updated baseUrl to '/' for Vercel deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   url: 'https://syedmaazhasan0.github.io', // Replace with your GitHub Pages URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-research-paper/', // Replace with your repo name
+  baseUrl: '/', // Replace with your repo name
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
